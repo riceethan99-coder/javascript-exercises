4f31a27d91f63c92f45d5d4d26d2c0db6006eb62
Ethan Rice <riceethan99@gmail.com>
2026-04-01 19:52:50 +0100

    lesson 7 javascript foundations

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+     let string1 = string.split("").reverse().join("")
+    return string1
 };
 
+reverseString("")
 // Do not edit below this line
 module.exports = reverseString;
