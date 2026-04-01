4dd5c6c5a7a95e1b6bc5a14cd807e1bda1d799f7
Ethan Rice <riceethan99@gmail.com>
2026-04-01 18:49:44 +0100

    Javascript foundation exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num >= 0) {
+    let result = ""
+    for(i = 0; i < num; i++) {
+        result = result + string
+    }
+    return result 
+} else {
+    return "ERROR"
+}
+}
 
-};
 
+repeatString("", 10)
 // Do not edit below this line
 module.exports = repeatString;
