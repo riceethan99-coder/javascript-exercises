ef940988b696fb6d096b49531c78d80af639d63e
Ethan Rice <riceethan99@gmail.com>
2026-04-01 20:54:23 +0100

    JS foundatio 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(arr, ...nums) {
+   return arr.filter(element => !nums.includes(element))
+}
+removeFromArray([1, 2, 3], "1", 3)
 // Do not edit below this line
 module.exports = removeFromArray;
