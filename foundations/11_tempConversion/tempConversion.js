71a92a6db231825ed29c279bf2fdafe1a0b53e8a
Ethan Rice <riceethan99@gmail.com>
2026-04-01 21:52:19 +0100

    JS foundation 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let celcius = (fahr - 32) * 5/9
+  return Number(celcius.toFixed(1))
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celc) {
+  let fahrenheit = (celc * 9 / 5) + 32
+  return Number(fahrenheit.toFixed(1))
 };
+
+
+convertToCelsius(-100)
+convertToFahrenheit(-10)
 
 // Do not edit below this line
 module.exports = {
