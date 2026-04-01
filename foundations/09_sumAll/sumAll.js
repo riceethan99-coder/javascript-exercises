61cd33e4db6d49f6b413066fcbf60cce115ae65d
Ethan Rice <riceethan99@gmail.com>
2026-04-01 21:18:09 +0100

    JS foundation 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let num3 = 0
+    
+    if(num1 > num2) {
+    let a = num1
+    num1 = num2 
+    num2 = a
+    }
+    
+    if(num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR"
+    }
+    
+    for(i = num1; i <= num2; i++) {
+        num3 = num3 + i
+    }
 
+    return num3
 };
 
+sumAll(10, [90, 1])
 // Do not edit below this line
 module.exports = sumAll;
