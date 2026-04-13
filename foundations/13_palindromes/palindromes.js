731283ff0592fdd3e428cdbffccd9eb38743d6ab
Ethan Rice <riceethan99@gmail.com>
2026-04-13 12:54:17 +0100

    Palindromes complete

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {
+const palindromes = function(word) {
+    word = word.toLowerCase()
+    word = word.replace(/[^a-z0-9]/g,'')
+    let arr = word.split("")
+    let reverseArray = arr.reverse()
+    let reversedWord = reverseArray.join("")
+    return word == reversedWord
 
 };
 
