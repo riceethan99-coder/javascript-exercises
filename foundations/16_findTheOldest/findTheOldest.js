f972bb8df72671b6f69df6ac2f0691577b828ecc
Ethan Rice <riceethan99@gmail.com>
2026-04-13 14:58:29 +0100

    findTheOldest exercise complete

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+   return array.reduce((oldest, currentPerson) => {
+        const oldestAge = oldest.yearOfDeath === undefined ? 2026 - oldest.yearOfBirth : oldest.yearOfDeath - oldest.yearOfBirth
+        const currentAge = currentPerson.yearOfDeath === undefined ? 2026 - currentPerson.yearOfBirth : currentPerson.yearOfDeath - currentPerson.yearOfBirth
+        return currentAge > oldestAge ? currentPerson : oldest
+   }, array[0])
 };
 
 // Do not edit below this line
