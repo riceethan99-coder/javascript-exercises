db45eb6a6f8cd9ad601cfe2f22ee3b420217540e
Ethan Rice <riceethan99@gmail.com>
2026-04-13 14:35:53 +0100

    GetTheTitle exercise complete

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.map(function(item){
+        return item.title
+    })
 };
 
 // Do not edit below this line
