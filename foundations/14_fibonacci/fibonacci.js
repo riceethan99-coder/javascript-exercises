ab68fb42740aa0990018941719f58e4686f0fbf0
Ethan Rice <riceethan99@gmail.com>
2026-04-13 14:20:00 +0100

    Fibonacci exercise complete

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    Number(num)
+    let num1 = 0
+    let num2= 1
+    let num3 = 0
+    if(num < 0) {
+        return "OOPS"
+    } else {
+    for(let a = 1; a <= num; a++) {
+        num3 = num1 + num2 
+        num1 = num2 
+        num2 = num3 
+    }
+    return num1
+} 
 };
 
 // Do not edit below this line
